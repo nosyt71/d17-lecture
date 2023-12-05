477f57272df2c77489460ea0124adb6f2b41e4e9
nosyt71 <tysonchan17@gmail.com>
2023-12-05 10:41:43 +0800

    first commit

---
+++ src/main/java/sg/edu/nus/iss/d17lecture/D17LectureApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.d17lecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class D17LectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(D17LectureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sg/edu/nus/iss/d17lecture/D17LectureApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.d17lecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class D17LectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
